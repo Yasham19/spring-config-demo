6076a4e0cf889717552511109e99e6cd05c554e2
Vmaheya <yasham.maheshwari@pearson.com>
2019-09-03 15:45:15 +0530

    config-client

---
+++ src/main/java/com/demo/config/configclient/MessageResource.java
@@ -0,0 +1,23 @@
+package com.demo.config.configclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/rest")
+@RestController
+@RefreshScope
+public class MessageResource {
+
+
+    @Value("${message}")
+    private String message;
+
+    @GetMapping("/message")
+    public String message(){
+        return message;
+    }
+
+}
